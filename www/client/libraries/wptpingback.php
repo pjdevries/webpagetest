cd20625bcd653567ccf4dde830cd7122778e8055
Pieter-Jan de Vries <pieter@obix.nl>
2022-10-16 17:08:25 +0200

    Writes pingback data to file.

--- www/client/libraries/wptpingback.php
+++ www/client/libraries/wptpingback.php
@@ -7,4 +7,5 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  * @link        https://www.obix.nl
  */
- printf("Test finished: %s\n", $_GET['id']);
+ file_put_contents(__DIR__ . '/wtppingbak.out.txt',
+	 sprintf("%s, %s\n",(new DateTime())->format('Y-m-d H:i:s'), $_GET['id']), FILE_APPEND | LOCK_EX);
